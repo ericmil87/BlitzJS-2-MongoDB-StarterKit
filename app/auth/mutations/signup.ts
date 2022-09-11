f787d21aab98f811bc7c3cc32c271e48b1403093
Eric Milfont <ericmilfont@milfont.net>
2022-09-11 17:11:15 -0300

    Fix: Unique e-mail sign up.

--- app/auth/mutations/signup.ts
+++ app/auth/mutations/signup.ts
@@ -5,6 +5,14 @@ import { Role } from "types"
 import { Signup } from "../validations"
 
 export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, ctx) => {
+  // 1. Get the user
+  const checkIfUserExists = await db.user.findFirst({ where: { email: email.toLowerCase() } })
+  if (checkIfUserExists) {
+    throw new Error(
+      "This e-mail is already associated with another account in our database. Please use another e-mail to sign up."
+    )
+  }
+
   const hashedPassword = await SecurePassword.hash(password.trim())
   const user = await db.user.create({
     data: { email: email.toLowerCase().trim(), hashedPassword, role: "USER" },
